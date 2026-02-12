7415b5a0bef0ace4a379c865ef85c4c51254a2e1
Maicol Salazar <stivsal12@gmail.com>
2026-02-11 21:44:21 -0500

    DB to Render

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -7,7 +7,8 @@ async function bootstrap() {
   app.enableCors({
     origin: '*',
   });
+  const port = process.env.PORT || 3000;
 
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(port, '0.0.0.0');
 }
 bootstrap();
